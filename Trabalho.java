0c098a9c1b5249900af7ed18e6df214c6b93c41e
Matheus <matheusbnedel@gmail.com>
2016-04-06 20:34:53 -0300

    Matrizes adicionadas

--- Trabalho.java
+++ Trabalho.java
@@ -1,2 +1,26 @@
-sdsofsdofdksofkdso
-siodfsodfoskdfosk
+public class Trabalho1 {
+	public static void main(String[] args) {
+
+		String A[][] = new String[4][3];
+		String B[][] = new String[4][3];
+		String C[][] = new String[4][3];
+		String D[][] = new String[4][3];
+
+		//A[0][0] = “EMPRESTE”;
+		// A[0][0] = 
+		// A[0][0] = 
+		// A[0][0] = 
+		// A[0][0] = 
+		// A[0][0] = 
+		// A[0][0] = 
+		// A[0][0] = 
+		// A[0][0] = 
+		// A[0][0] = 
+		// A[0][0] = 
+		// A[0][0] = 
+
+
+
+
+	}
+}
---
+++ Trabalho1.java
@@ -0,0 +1,53 @@
+public class Trabalho1 {
+	public static void main(String[] args) {
+
+		String Compartimentos[][] = new String[4][3];
+		String Artistas[][] = new String[4][3];
+		String Musicas[][] = new String[4][3];
+		int D[][] = new int[4][3];
+
+		Compartimentos[0][0] = "101";
+		Compartimentos[0][1] = "102";
+		Compartimentos[0][2] = "103";
+		Compartimentos[1][0] = "201";
+		Compartimentos[1][1] = "202";
+		Compartimentos[1][2] = "203";
+		Compartimentos[2][0] = "301";
+		Compartimentos[2][1] = "302";
+		Compartimentos[2][2] = "303";
+		Compartimentos[3][0] = "401";
+		Compartimentos[3][1] = "402";
+		Compartimentos[3][2] = "403";
+
+		Artistas[0][0] = "Kanye West";
+		Artistas[0][1] = "Kanye West";
+		Artistas[0][2] = "Kanye West";
+		Artistas[1][0] = "Bon Jovi";
+		Artistas[1][1] = "Bon Jovi";
+		Artistas[1][2] = "Bon Jovi";
+		Artistas[2][0] = "Ben Harper";
+		Artistas[2][1] = "Ben Harper";
+		Artistas[2][2] = "Ben Harper";
+		Artistas[3][0] = "Jack Johnson";
+		Artistas[3][1] = "Jack Johnson";
+		Artistas[3][2] = "Jack Johnson";
+
+		Musicas[0][0] = "Golddigger";
+		Musicas[0][1] = "Homecoming";
+		Musicas[0][2] = "Can't Tell Me Nothing";
+		Musicas[1][0] = "It's My Life";
+		Musicas[1][1] = "Living on a Prayer";
+		Musicas[1][2] = "You Give Love a Bad Name";
+		Musicas[2][0] = "Amen Omen";
+		Musicas[2][1] = "Diamonds on The Inside";
+		Musicas[2][2] = "Everything";
+		Musicas[3][0] = "Upside Down";
+		Musicas[3][1] = "Better Together";
+		Musicas[3][2] = "Banana Pancakes";
+
+		//1)Solicite ao usuário o número de vezes que a música foi pedida(TOCADA), a mensagem que aparece para o usuário deve seguir o seguinte modelo:
+
+
+
+	}
+}
