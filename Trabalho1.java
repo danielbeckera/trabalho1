7aae02f70acbf0a5e75c7ee7c9aeda1f83437720
Matheus Berkenbrock Nedel <matheusbnedel@gmail.com>
2016-04-07 01:00:38 -0300

    Mais exercícios adicionados

--- Trabalho1.java
+++ Trabalho1.java
@@ -1,10 +1,12 @@
+import javax.swing.JOptionPane;
+
 public class Trabalho1 {
 	public static void main(String[] args) {
 
 		String Compartimentos[][] = new String[4][3];
 		String Artistas[][] = new String[4][3];
 		String Musicas[][] = new String[4][3];
-		int D[][] = new int[4][3];
+		int Registro[][] = new int[4][3];
 
 		Compartimentos[0][0] = "101";
 		Compartimentos[0][1] = "102";
@@ -45,9 +47,48 @@ public class Trabalho1 {
 		Musicas[3][1] = "Better Together";
 		Musicas[3][2] = "Banana Pancakes";
 
-		//1)Solicite ao usuÃ¡rio o nÃºmero de vezes que a mÃºsica foi pedida(TOCADA), a mensagem que aparece para o usuÃ¡rio deve seguir o seguinte modelo:
+		//1)Solicite ao usuário o número de vezes que a música foi pedida(TOCADA), a mensagem que aparece para o usuário deve seguir o seguinte modelo:
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 3; j++) {
+				Registro[i][j] = (int) (Math.random()*31);
+				System.out.print(Registro[i][j] + "\t");
+			}
+			System.out.println();
+		}
 
+		//2) Solicite ao usuário que informe um nome de um dos artista (STRING) e utilizando PESQUISA LINEAR mostre o número de músicas que este artista possui cadastrado no sistema da JUKEBOX.
+		String artistaSelecionado = JOptionPane.showInputDialog(null, "Informe um artista para saber quantas músicas ele possui na jukebox.");
+		int counter = 0;
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 3; j++) {
+				if(Artistas[i][j].equals(artistaSelecionado))
+					counter++;
+			}
+		}
+		JOptionPane.showMessageDialog(null, "O artista possui " + counter + " músicas nesta jukebox.");
 
+		//3) Mostre quantas vezes a máquina tocou músicas. Como cada música custa a moeda de R$ 1,00 , o dono deseja saber quantos reais tem em caixa.
+		int totalMusicas = 0;
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 3; j++) {
+				totalMusicas += Registro[i][j];
+			}
+		}
+		JOptionPane.showMessageDialog(null, "A jukebox tocou " + totalMusicas + " músicas, portanto possui R$" + totalMusicas + " em caixa.");
+
+		//4) Mostre o nome do artista, o número do compartimento e a música MAIS PEDIDA.
+		int linha = 0, coluna = 0, musicaMaisTocada = 0;
+
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 3; j++) {
+				if(Registro[i][j] > musicaMaisTocada) {
+					musicaMaisTocada = Registro[i][j];
+					linha = i;
+					coluna = j;
+				}
+			}
+		}
+		JOptionPane.showMessageDialog(null, "A música mais tocada é " + Musicas[linha][coluna] + " do artista " + Artistas[linha][coluna] + " que corresponde ao compartimento " + Compartimentos[linha][coluna] + ".");
 
 	}
 }
